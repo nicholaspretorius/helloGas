0d0edbaa112e1d283a8435c2cab653f56013a735
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-08-24 13:12:28 +0200

    Initial commit. Getting started with Clasp and GAS.

---
+++ index.ts
@@ -0,0 +1,16 @@
+import "google-apps-script";
+
+/** 
+ * Function to test something
+ *
+ * @param {string} str The string to modify
+ * @return Logs out the Index counter and the value.
+ * @customfunction
+ */
+function hello(name: String) {
+    Logger.log(`Hello ${name}`);
+}
+
+
+
+
